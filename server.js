555403cb4235275cf60f1c4d2ba685eca7cc97b7
anus26 <anusraza268gmail.com>
2025-05-28 18:52:00 -0700

    modified

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.use("/message",messagerouter)
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-server.PORT, () => {
+server.listen, () => {
    console.log(`⚙️  Server is running at port : ${PORT}`);
 };
